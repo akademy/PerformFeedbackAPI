1efbe371b392e26de10e6129e6b0db1694c33c58
Matthew Wilcoxson <git@akademy.co.uk>
2017-08-21 22:36:53 +0100

    Configuration file

---
+++ config/config.js
@@ -0,0 +1,7 @@
+const configLocal = require( './config.local');
+
+const config = {
+	local: configLocal
+};
+
+module.exports = config;
---
+++ config/config.local.template.js
@@ -0,0 +1,7 @@
+const configLocal = {
+	api: {
+		'key-uuid' : /s/  // e.g. 00000000-0000-4000-0000-00000000000
+	}
+};
+
+module.exports = configLocal;

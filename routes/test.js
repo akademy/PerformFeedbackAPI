619072676f5e26053e23c567b16e67350c767860
Matthew Wilcoxson <git@akademy.co.uk>
2017-08-21 11:54:08 +0100

    Add simple test urls

--- routes/test.js
+++ routes/test.js
@@ -5,10 +5,29 @@ router.get('/', (req, res, next) => {
 	res.send(
 		`
 		<html><body>
+			<hr/>
+			
 			<ul>
-				<li><a href="/test/q">GET q</a></li>
-				<li><form action="/test/q" method="post"><input name="A" type="hidden"/><input type="submit" value="POST q"/></form></li>
+				<li><a href="/api/pre">GET api/pre</a></li>
 			</ul>
+			<hr/>
+			
+			<ul>
+				<li><a href="/api/live">GET api/live</a></li>
+			</ul>
+			<hr/>
+			
+			<ul>
+				<li><a href="/api/post">GET api/pre</a></li>
+			</ul>
+			<hr/>
+			
+			<ul>
+				<li><a href="/test/q">GET test/q</a></li>
+				<li><form action="/test/q" method="post"><input name="A" type="hidden"/><input type="submit" value="POST test/q"/></form></li>
+			</ul>
+			<hr/>
+			
 		</body></html>
 		`
 	);

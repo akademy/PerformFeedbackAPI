68e5fcf5cebff9e07aea3f11f67eb2df81bb3bbb
Matthew Wilcoxson <bitbucket@akademy.co.uk>
2017-08-28 16:50:14 +0100

    Syncing of live feedbacks

--- routes/api/live.js
+++ routes/api/live.js
@@ -1,8 +1,49 @@
 const express = require('express'),
 	router = express.Router();
 
+
+const api = require( "../../lib/api" ),
+	mongo = require( "../../lib/mongo"),
+	config = require("../../config/config");
+
 router.get('/', (req, res, next) => {
 	res.json( {API: 'live'} );
 });
 
-module.exports = router;
+router.post('/', (req, res) => {
+	if( api.requestRespond( req.body, res ) ) {
+		console.log("api/live", req.body);
+
+		mongo.upsert(
+			config.mongo.collections.live,
+			{ randomUuid: req.body.randomUuid, feedbackId: req.body.payload.feedbackId },
+			mongo.prepareUpsert( req.body.payload, req.body.requestDateTime),
+			( error, data ) => {
+				if( !error ) {
+					let response = api.responseSetup( req.body.requestId );
+
+					response.payload = {
+						updated : true
+					};
+
+					res.json( response );
+				}
+			}
+		)
+	}
+});
+
+/* Always:
+	request: {
+		appOs: OS System
+		appVersion: current app version,
+		keyUuid: server generate uuid,
+		randomUuid: app generate uuid,
+		requestUuid: request generated uuid,
+		payload: ...
+	};
+	result: {
+		resultUuid: must match request-uuid,
+	};
+
+*/module.exports = router;

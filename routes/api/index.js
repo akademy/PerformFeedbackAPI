940aae93454d6ed70c76156b368c3f1e50b86c8f
Matthew Wilcoxson <git@akademy.co.uk>
2017-09-04 12:17:50 +0100

    Show a simple page

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,16 +1,30 @@
 const express = require('express'),
 	router = express.Router();
 
-const pre = require('./pre');
-const live = require('./live');
-const post = require('./post');
+const api = require( "../../lib/api" ),
+	mongo = require( "../../lib/mongo"),
+	config = require("../../config/config");
 
-router.get('/', (req, res, next) => {
-	res.json( {API: ''} );
-});
+const pre = require('./pre'),
+		live = require('./live'),
+		post = require('./post');
 
 router.use( '/pre', pre );
 router.use( '/live', live );
 router.use( '/post', post );
+
+/*router.get('/', (req, res, next) => {
+	res.json( {API: ''} );
+});*/
+
+router.get('/', (req, res, next) => {
+	mongo.select( (error, data ) => {
+		res.send( `
+			<html><body>
+				${data}
+			</body></html>
+		` );
+	});
+});
 
 module.exports = router;

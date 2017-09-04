0a535ef47b001c6ba7bb5b4e4efe13e1d03aca3a
Matthew Wilcoxson <git@akademy.co.uk>
2017-09-04 12:49:26 +0100

    Show a simple page of data

--- routes/api/index.js
+++ routes/api/index.js
@@ -19,9 +19,79 @@ router.use( '/post', post );
 
 router.get('/', (req, res, next) => {
 	mongo.select( (error, data ) => {
+
+		let body1 = [];
+		let keys = [];
+		if( data.pre.length > 0) {
+			keys = Object.keys( data.pre[0] );
+		}
+		else {
+			body1.push("<p>Empty</p>");
+		}
+
+		data.pre.forEach( (item) => {
+			keys.forEach( (key) => {
+				if( key in item ) {
+					body1.push(`<p><b>${key}</b>:${item[key]}</p>`);
+				}
+			});
+			body1.push( "<hr/>" );
+		} );
+
+		body1 = body1.join("");
+
+		let body2= [];
+		keys = [];
+		if( data.live.length > 0) {
+			keys = Object.keys( data.live[0] );
+		}
+		else {
+			body2.push("<p>Empty</p>");
+		}
+
+		data.live.forEach( (item) => {
+			keys.forEach( (key) => {
+				if( key in item ) {
+					body2.push(`<p><b>${key}</b>:${item[key]}</p>`);
+				}
+			});
+			body2.push( "<hr/>" );
+		} );
+
+		body2 = body2.join("");
+
+		let body3= [];
+		keys = [];
+		if( data.post.length > 0) {
+			keys = Object.keys( data.post[0] );
+		}
+		else {
+			body3.push("<p>Empty</p>");
+		}
+
+		data.post.forEach( (item) => {
+			keys.forEach( (key) => {
+				if( key in item ) {
+					body3.push(`<p><b>${key}</b>:${item[key]}</p>`);
+				}
+			});
+			body3.push( "<hr/>" );
+		} );
+
+		body3 = body3.join("");
+
 		res.send( `
 			<html><body>
-				${data}
+				<h1>Data</h1>
+				<h2>Profile</h2>
+				${body1}
+				<hr/>
+				<h2>Live</h2>
+				${body2}
+				<hr/>
+				<h2>Post</h2>
+				${body3}
+				<hr/>
 			</body></html>
 		` );
 	});

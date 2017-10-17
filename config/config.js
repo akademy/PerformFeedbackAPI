36613c8bb6fc2d9d503601f8af3902c69298c6b0
Matthew Wilcoxson <git@akademy.co.uk>
2017-10-18 00:27:39 +0100

    Further analysis

--- config/config.js
+++ config/config.js
@@ -3,14 +3,17 @@ const configLocal = require( './config.local');
 const config = {
 	local: configLocal,
 	mongo: {
-		database: "performFeeback1",
+		database: "performFeedback",
 		collections: {
 			pre: "pre",
 			live: "live",
 			post: "post",
 			private: "private",
 			combined : "combined",
-			reduced: "reduced"
+			reduced: "reduced",
+			offset: "offset",
+			flatten: "flatten",
+			flattenLive: "flattenLive"
 		}
 	}
 };
